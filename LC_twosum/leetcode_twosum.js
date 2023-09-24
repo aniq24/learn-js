ea0749700bf4f4fcd4c725ee479e1ed73152ab73
aniq24 <aniq24@gmail.com>
2023-09-24 20:50:08 +0800

    modified the leetcode_twosum.js to be able to accept number input and target input

--- LC_twosum/leetcode_twosum.js
+++ LC_twosum/leetcode_twosum.js
@@ -1,19 +1,26 @@
-let numberInput =  document.getElementById('numberInput');
+let numberInput = document.getElementById('numberInput');
 let trgtInput = document.getElementById('trgtInput');
 let numberBtn = document.getElementById('inputBtn');
 let trgtBtn = document.getElementById('trgtBtn');
 let arrayResult = document.getElementById('arrayList');
+let targetResult = document.getElementById('target');
 
 const array = [];
 
+
 function inputBtnClicked(){
 
-        array.push(numberInput);
-        arrayResult.innerHTML = array;
+
+    array.push(parseInt(numberInput.value));
+    arrayResult.innerHTML = "Array: " + array.join(', ');
+
 }
 
-trgtBtn.addEventListener('click', inputBtnClicked(){
+function trgtBtnClicked(){
 
+   targetResult.innerHTML = "Target: " + parseInt(trgtInput.value);
 
+}
 
-)
+numberBtn.addEventListener('click', inputBtnClicked);
+trgtBtn.addEventListener('click', trgtBtnClicked);
