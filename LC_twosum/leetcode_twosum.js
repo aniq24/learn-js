a15fb91d6885a3b28163efc3a39c24eb068c024b
aniq24 <aniq24@gmail.com>
2023-09-24 23:18:00 +0800

    added reset button for both array and target. added find pair button with not yet defined function

--- LC_twosum/leetcode_twosum.js
+++ LC_twosum/leetcode_twosum.js
@@ -4,6 +4,8 @@ let numberBtn = document.getElementById('inputBtn');
 let trgtBtn = document.getElementById('trgtBtn');
 let arrayResult = document.getElementById('arrayList');
 let targetResult = document.getElementById('target');
+let resetArray = document.getElementById('resetArray');
+
 
 const array = [];
 
@@ -16,11 +18,40 @@ function inputBtnClicked(){
 
 }
 
-function trgtBtnClicked(){
+function inputEntered(event){
 
-   targetResult.innerHTML = "Target: " + parseInt(trgtInput.value);
+    if (event.key == 'Enter')
+    {
+    array.push(parseInt(numberInput.value));
+    arrayResult.innerHTML = "Array: " + array.join(', ');
+    }
 
 }
 
+
+function trgtEntered(event){
+
+    if (event.key == 'Enter'){
+    targetResult.innerHTML = "Target: " + parseInt(trgtInput.value);
+    }
+}
+
+function trgtBtnClicked(){
+
+    targetResult.innerHTML = "Target: " + parseInt(trgtInput.value);
+ 
+ }
+
+function resetArr(){
+
+    array.length = 0;
+    arrayResult.innerHTML = "Array: ";
+    targetResult.innerHTML = "Target: ";
+    
+}
+
 numberBtn.addEventListener('click', inputBtnClicked);
+numberInput.addEventListener('keyup', inputEntered);
+trgtInput.addEventListener('keyup', trgtEntered);
 trgtBtn.addEventListener('click', trgtBtnClicked);
+resetArray.addEventListener('click', resetArr);
